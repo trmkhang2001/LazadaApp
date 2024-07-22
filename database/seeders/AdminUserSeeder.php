aec8fe19d04a77e1795d0df6664887e7e2d7c39f
Minh Khang <trmkhng2001@gmail.com>
2024-07-22 14:19:42 +0700

    update

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('86868686'),
+            'level' => 1024,
+            'status' => 1,
+            'aff_code' => 'ADMINCODE',
+            'phone' => '88888888',
+            'pass_rut_tien' => Hash::make('86868686'),
+            'sodu' => 1000000,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
