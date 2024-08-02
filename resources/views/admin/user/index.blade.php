a09d981e3def235fd71148550a738b7c1c00808d
Minh Khang <trmkhng2001@gmail.com>
2024-08-02 10:03:45 +0700

    update

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -47,90 +47,178 @@
                 </thead>
                 <tbody>
                     @foreach ($items as $user)
-                        <tr>
-                            <th scope="row">{{ $user->aff_code }}</th>
-                            <td>
-                                <div class="cell">
-                                    <span class="el-tag el-tag--success el-tag--mini el-tag--light">Bên
-                                        ngoài</span>
-                                    <div>
-                                        <a style="color: red;">{{ $user->phone }}</a>
-                                        <div>Mã số cá nhân: 411068</div>
-                                        <div class="text-primary">Cấp độ hiện tại: VIP1</div>
+                        @if (Auth::user()->level == 1024)
+                            <tr>
+                                <th scope="row">{{ $user->aff_code }}</th>
+                                <td>
+                                    <div class="cell">
+                                        <span class="el-tag el-tag--success el-tag--mini el-tag--light">Bên
+                                            ngoài</span>
+                                        <div>
+                                            <a style="color: red;">{{ $user->phone }}</a>
+                                            <div>Mã số cá nhân: 411068</div>
+                                            <div class="text-primary">Cấp độ hiện tại: VIP1</div>
+                                        </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div class="text-danger">{{ '₫' . number_format($user->sodu) }}</div>
+                                        <div><a class="text-primary"><span class="el-link--inner">Điểm tín
+                                                    dụng: 60</span></a></div>
+                                        <div><a class="text-primary"><span class="">Giá
+                                                    trị rủi ro: 0</span></a></div>
                                     </div>
-                            </td>
-                            <td>
-                                <div class="cell">
-                                    <div class="text-danger">{{ '₫' . number_format($user->sodu) }}</div>
-                                    <div><a class="text-primary"><span class="el-link--inner">Điểm tín
-                                                dụng: 60</span></a></div>
-                                    <div><a class="text-primary"><span class="">Giá
-                                                trị rủi ro: 0</span></a></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="cell">
-                                    <span class="text-danger">0</span>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="">
-                                    <span class="text-success">Đại lý: {{ $user->aff_code }}</span>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="cell">
-                                    <div>Hoa hồng ngày hôm nay: 0 / 4Lần</div>
-                                    <div>Hoa hồng tích lũy: 4 / 10586.96Lần</div>
-                                    <div>Thu nhập của ngày hôm qua: 10586.96</div>
-                                    <div>Hoa hồng khuyến mãi tích lũy: 0</div>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="cell">
-                                    <div>Quyền hạn nhận đơn: <span class="text-success">Mở</span></div>
-                                    <div>Giám sát đơn hàng: <span class="text-danger">Cấm</span></div>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="cell">
-                                    <div>Rút tiền hàng ngày: ₫0</div>
-                                    <div>Tổng số tiền rút: ₫0</div>
-                                    <div>Nạp tiền hàng ngày: ₫0</div>
-                                    <div>Tổng số tiền nạp: ₫0</div>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="cell">8</div>
-                            </td>
-                            <td>
-                                <div class="cell">
-                                    <div>{{ $user->created_at }}</div>
-                                    <div>IP: 27.64.108.76</div>
-                                    <div>Địa chỉ IP: (105.841171,21.0245)</div>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="cell">
-                                    <div class="text-red">{{ $user->created_at }}</div>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="d-flex flex-column">
-                                    <div class="mb-2">
-                                        <a href="{{ route('admin.user.edit', $user->id) }}"
-                                            class="my-2 me-5 p-2 btn-yellow">
-                                            Sửa
-                                        </a>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <span class="text-danger">0</span>
                                     </div>
+                                </td>
+                                <td>
                                     <div class="">
-                                        <a href="" class="my-2 me-5 p-2 btn-blue">
-                                            Xoá
-                                        </a>
+                                        <span class="text-success">Đại lý: {{ $user->aff_code }}</span>
                                     </div>
-                                </div>
-                            </td>
-                        </tr>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div>Hoa hồng ngày hôm nay: 0 / 4Lần</div>
+                                        <div>Hoa hồng tích lũy: 4 / 10586.96Lần</div>
+                                        <div>Thu nhập của ngày hôm qua: 10586.96</div>
+                                        <div>Hoa hồng khuyến mãi tích lũy: 0</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div>Quyền hạn nhận đơn: <span class="text-success">Mở</span></div>
+                                        <div>Giám sát đơn hàng: <span class="text-danger">Cấm</span></div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div>Rút tiền hàng ngày: ₫0</div>
+                                        <div>Tổng số tiền rút: ₫0</div>
+                                        <div>Nạp tiền hàng ngày: ₫0</div>
+                                        <div>Tổng số tiền nạp: ₫0</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">8</div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div>{{ $user->created_at }}</div>
+                                        <div>IP: 27.64.108.76</div>
+                                        <div>Địa chỉ IP: (105.841171,21.0245)</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div class="text-red">{{ $user->created_at }}</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="d-flex flex-column">
+                                        <div class="mb-2">
+                                            <a href="{{ route('admin.user.edit', $user->id) }}"
+                                                class="my-2 me-5 p-2 btn-yellow">
+                                                Sửa
+                                            </a>
+                                        </div>
+                                        <div class="">
+                                            <a href="" class="my-2 me-5 p-2 btn-blue">
+                                                Xoá
+                                            </a>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endif
+                        @if (Auth::user()->level == 1000 && $user->level != 1024 && $user->level != 1000)
+                            <tr>
+                                <th scope="row">{{ $user->aff_code }}</th>
+                                <td>
+                                    <div class="cell">
+                                        <span class="el-tag el-tag--success el-tag--mini el-tag--light">Bên
+                                            ngoài</span>
+                                        <div>
+                                            <a style="color: red;">{{ $user->phone }}</a>
+                                            <div>Mã số cá nhân: 411068</div>
+                                            <div class="text-primary">Cấp độ hiện tại: VIP1</div>
+                                        </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div class="text-danger">{{ '₫' . number_format($user->sodu) }}</div>
+                                        <div><a class="text-primary"><span class="el-link--inner">Điểm tín
+                                                    dụng: 60</span></a></div>
+                                        <div><a class="text-primary"><span class="">Giá
+                                                    trị rủi ro: 0</span></a></div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <span class="text-danger">0</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="">
+                                        <span class="text-success">Đại lý: {{ $user->aff_code }}</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div>Hoa hồng ngày hôm nay: 0 / 4Lần</div>
+                                        <div>Hoa hồng tích lũy: 4 / 10586.96Lần</div>
+                                        <div>Thu nhập của ngày hôm qua: 10586.96</div>
+                                        <div>Hoa hồng khuyến mãi tích lũy: 0</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div>Quyền hạn nhận đơn: <span class="text-success">Mở</span></div>
+                                        <div>Giám sát đơn hàng: <span class="text-danger">Cấm</span></div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div>Rút tiền hàng ngày: ₫0</div>
+                                        <div>Tổng số tiền rút: ₫0</div>
+                                        <div>Nạp tiền hàng ngày: ₫0</div>
+                                        <div>Tổng số tiền nạp: ₫0</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">8</div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div>{{ $user->created_at }}</div>
+                                        <div>IP: 27.64.108.76</div>
+                                        <div>Địa chỉ IP: (105.841171,21.0245)</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="cell">
+                                        <div class="text-red">{{ $user->created_at }}</div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="d-flex flex-column">
+                                        <div class="mb-2">
+                                            <a href="{{ route('admin.user.edit', $user->id) }}"
+                                                class="my-2 me-5 p-2 btn-yellow">
+                                                Sửa
+                                            </a>
+                                        </div>
+                                        <div class="">
+                                            <a href="" class="my-2 me-5 p-2 btn-blue">
+                                                Xoá
+                                            </a>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endif
                     @endforeach
                 </tbody>
             </table>
