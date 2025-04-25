0dad1f973c2224169dad84ab7f9f54db0e0959d6
Minh Khang <trmkhang2001@gmail.com>
2025-04-25 13:13:27 +0700

    update

--- app/Http/Controllers/DonHangController.php
+++ app/Http/Controllers/DonHangController.php
@@ -23,10 +23,12 @@ class DonHangController extends Controller
                 ->with('user')
                 ->with('don_hang_maus')
                 ->paginate(10);
-                $sodu = Auth::user()->sodu;
+                $user = Auth::user();
+        $sodu = $user->sodu;
         } elseif (Auth::user()->level == 1024) {
             $orders = DonHang::orderBy('created_at', 'desc')->with('user')->with('don_hang_maus')->paginate(10);
-            $sodu = Auth::user()->sodu;
+            $user = Auth::user();
+        $sodu = $user->sodu;
         }
         return view('admin.orders.index', compact('orders', 'don_hang_maus','sodu'));
     }
